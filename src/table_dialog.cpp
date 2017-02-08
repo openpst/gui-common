1539f04f3c8bf205000f3f0934cb763d1c06b118
Gassan Idriss <ghassani@gmail.com>
2017-02-08 13:00:30 -0800

    license

--- src/table_dialog.cpp
+++ src/table_dialog.cpp
@@ -1,13 +1,30 @@
 /**
-* LICENSE PLACEHOLDER
+*
+* (c) Gassan Idriss <ghassani@gmail.com>
+* 
+* This file is part of gui-common.
+* 
+* gui-common is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* gui-common is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with gui-common. If not, see <http://www.gnu.org/licenses/>.
 *
 * @file table_dialog.cpp
 * @class TableDialog
-* @package OpenPST
-* @brief An abstract dialog which allows programs to create a popup with a table view
+* @package openpst/gui-common
+* @brief
 *
 * @author Gassan Idriss <ghassani@gmail.com>
 */
+
 #include "table_dialog.h"
 #include "ui_table_dialog.h"
 
